cf5cc56941dd3c019f599ff4972f7bb2d7d2fd91
Lucas Simoes <lcsimoes@yahoo.com>
2024-07-26 16:25:21 -0300

    fix build error

--- src/app/categorias/page.tsx
+++ src/app/categorias/page.tsx
@@ -5,6 +5,25 @@ import { CategoryService } from "@/services/categories";
 
 import CategoryCard from "@/components/CategoryCard";
 
+const categories = [
+    {
+        category: "Informática",
+        value: "informatica"
+    },
+    {
+        category: "Mobília",
+        value: "mobilia"
+    },
+    {
+        category: "Telefonia",
+        value: "telefonia"
+    },
+    {
+        category: "Audiovisual",
+        value: "audiovisual"
+    }
+]
+
 export default function Categorias(): ReactElement {
 
     const { data, isLoading } = CategoryService.get()
@@ -17,7 +36,7 @@ export default function Categorias(): ReactElement {
             <h1 className="text-2xl font-semibold"> Lista de Categorias </h1>
             <p className="mb-5 opacity-60 max-w-[600px] mt-2"> Clique no card referente à categoria que deseja pesquisar </p>
             <div className="grid grid-cols-1 xl:grid-cols-2 xl:grid-rows-2 gap-8 mt-16">
-                { data.map(category => (
+                { categories.map(category => (
                     <CategoryCard key={category.category} path={category.value} name={category.category}/>
                 )) }
             </div>
